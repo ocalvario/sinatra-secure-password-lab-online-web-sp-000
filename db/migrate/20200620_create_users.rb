bb148ebe807b2b69497c8796b00ebdc6b45449a6
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-18 23:16:07 +0000

    Automatically backed up by Learn

--- db/migrate/20200620_create_users.rb
+++ db/migrate/20200620_create_users.rb
@@ -1,8 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+
+class AddPasswordDigest < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :username
-      t.string :password
-    end
+    add_column :users, :password_digest, :string
   end
 end
